635ed7b5df683fd82313c925cf6a590110620866
Edson Caires <edsoncaires17@gmail.com>
2025-08-13 21:29:59 -0300

    Create aula1.6.kt
    Usando operações no programa.

---
+++ aula1.6.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var cliente  = 11 
+    
+    cliente += 7
+    println("Com a saída: ${cliente} clientes")
+    
+    cliente -= 3
+    println("Com a chegada: ${cliente} clientes")
+    
+    cliente *= 2
+    println("Com a chegada: ${cliente} clientes")
+    
+    cliente /= 3
+    println("Com a chegada: ${cliente} clientes")
+    
+}

db6ce14f180dc140f8b433438f42d3a0123d027b
Edson Caires <edsoncaires17@gmail.com>
2025-08-13 21:26:10 -0300

    Create aula1.5.kt
    contagem de clientes

---
+++ aula1.5.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var cliente  = 10 
+    
+    cliente  = cliente - 2
+    println("Com a saída: ${cliente} clientes")
+    
+    cliente = cliente + 3
+    println("Com a chegada: ${cliente} clientes")

4242d070f51090e46247b29c38a2d4703abcbdc4
Edson Caires <edsoncaires17@gmail.com>
2025-08-13 21:20:29 -0300

    Create aula1,4.kt
    Tipos de variáveis

---
+++ aula1,4.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val a = int = 1000 
+    val b = string = "log message" 
+    val c = double = 3.14 
+    val d = log = 100_000_000_000_000
+    val e = boolean = false
+    val f = char = '\n'
+}

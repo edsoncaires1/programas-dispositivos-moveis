51053f07a1d53c778cb00de6cfd913f885c8a978
Edson Caires <edsoncaires17@gmail.com>
2025-08-20 20:19:23 -0300

    Create aula 2.3.kt
    Lista de frutas, essa eu não entendi muito bem.

---
+++ aula 2.3.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val ReadFruits = setOf("Kiwi", "Limão", "Cereja", "Laranja", "Laranja")
+    println(ReadFruits)
+    
+    val Fruits: MutableSet<String> = mutableSetOf("Kiwi", "Limão", "Cereja", "Cereja", "Laranja")
+    println(Fruits)
+}

856e0f6b9ad793d9edd6012e8615375b6294cc9c
Edson Caires <edsoncaires17@gmail.com>
2025-08-20 19:51:18 -0300

    Create aula2.kt
    Lista de frutas em Kotlin. Aula 02

---
+++ aula2.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val fruits = listOf("Maçã", "Laranja","Banana", "Limão")
+    
+    println(fruits [3])
+    println(fruits [0])
+    println(fruits [2])
+    println(fruits [1])
+    
+    println(fruits.size)
+    
+    println(fruits.contains("Banana"))
+}

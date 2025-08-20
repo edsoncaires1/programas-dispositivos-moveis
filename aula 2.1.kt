9e0b156e15f22d18fd13e47fe1438003c8e7dd2e
Edson Caires <edsoncaires17@gmail.com>
2025-08-20 20:01:54 -0300

    Create aula 2.1.kt
    Lista iteração elegante, adicionei um println onde destaca a fruta favorita.

---
+++ aula 2.1.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val fruits = listOf("Maçã", "Laranja","Banana", "Limão")
+    
+    for (i in 0 until fruits.size){
+        println("Fruta #$i: ${fruits[i]}")
+}
+    println("eu gosto da ${fruits[0]}")
+}

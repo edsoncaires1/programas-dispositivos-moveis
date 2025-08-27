3b5fe2a1b8844d50715769ea23d3a8749c4d5d85
Edson Caires <edsoncaires17@gmail.com>
2025-08-27 20:44:14 -0300

    Create aula3.2.kt

---
+++ aula3.2.kt
@@ -0,0 +1,7 @@
+fun MensagemcomAutor( mensagem: String, autor: String, mensagem2: String){
+    println("[$autor] $mensagem $mensagem2 ")
+}
+
+fun main(){
+    MensagemcomAutor(autor = "Edson", mensagem = "Bom trabalho!", mensagem2 = "Vai Corinthians!")
+}
